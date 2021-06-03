ca6ac4c099d477ceda98ade3bdc74f3c5c840719
zsolt <gergelyzsolt2001@gmail.com>
2021-06-03 18:24:13 +0300

    Changed to new features

--- app/src/main/java/com/example/keystrokeauthenticationdemo/MainActivity.kt
+++ app/src/main/java/com/example/keystrokeauthenticationdemo/MainActivity.kt
@@ -1,37 +1,55 @@
 package com.example.keystrokeauthenticationdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.keystrokeauthentication.KeystrokeAuthenticationProvider
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
 
-    private lateinit var startButton: Button
-    private lateinit var stopButton: Button
-    private lateinit var authenticateButton: Button
-    private lateinit var pinEditText: EditText
-    private lateinit var trainButton: Button
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var authenticateButton:Button
+    private lateinit var pinEditText:EditText
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+    private lateinit var ks: KeystrokeAuthenticationProvider
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        stopButton = findViewById<Button>(R.id.stopButton)
         authenticateButton = findViewById<Button>(R.id.authenticate)
         pinEditText = findViewById<EditText>(R.id.editTextTextPersonName)
-        trainButton = findViewById<Button>(R.id.trainButton)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.nav_view)
+        toolbar = findViewById(R.id.toolbar)
 
+        //setSupportActionBar(toolbar)
 
-        var ks = KeystrokeAuthenticationProvider(this, pinEditText)
+        navigationView.bringToFront()
+        var toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        navigationView.setNavigationItemSelectedListener(this)
 
-        stopButton.setOnClickListener{
-            ks.stopBackgroundProcess()
-        }
+        ks = KeystrokeAuthenticationProvider(this.applicationContext, pinEditText)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
 
         authenticateButton.setOnClickListener{
             val authenticationResult = ks.authenticate()
@@ -42,11 +60,28 @@ class MainActivity : AppCompatActivity() {
             }
             Log.v("AUTHENTICATIONPROVIDER",authenticationResult.toString())
         }
+    }
 
-        trainButton.setOnClickListener{
-            ks.train()
+    override fun onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
         }
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
+        when(item.itemId){
+            R.id.nav_train -> ks.train()
+            R.id.nav_color -> {
+                ks.setKeyboardColor()
+            }
+            R.id.nav_deviation -> ks.setDeviationForThreshold()
+            R.id.nav_help -> ks.Help()
+        }
+
+        return true
+    }
 }
