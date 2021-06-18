876548bbd94d7f97b937366d85ba7ad32c6df07e
zsolt <gergelyzsolt2001@gmail.com>
2021-06-18 14:34:34 +0300

    changed authentication result and icon

--- app/src/main/java/com/example/keystrokeauthenticationdemo/MainActivity.kt
+++ app/src/main/java/com/example/keystrokeauthenticationdemo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.keystrokeauthenticationdemo
 
+import android.R.attr.delay
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -23,6 +27,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var navigationView: NavigationView
     private lateinit var toolbar: Toolbar
     private lateinit var ks: KeystrokeAuthenticationProvider
+    private lateinit var goodResultTextView: TextView
+    private lateinit var badPINResultTextView: TextView
+    private lateinit var badRhythmResultTextView: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +41,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.nav_view)
         toolbar = findViewById(R.id.toolbar)
+        goodResultTextView = findViewById(R.id.goodResultTextView)
+        badPINResultTextView = findViewById(R.id.badPINResultTextView)
+        badRhythmResultTextView = findViewById(R.id.badRhythmResultTextView)
 
         //setSupportActionBar(toolbar)
 
         navigationView.bringToFront()
-        var toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        var toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
@@ -52,13 +68,29 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onStart()
 
         authenticateButton.setOnClickListener{
+            goodResultTextView.visibility = View.INVISIBLE
+            badPINResultTextView.visibility = View.INVISIBLE
+            badRhythmResultTextView.visibility = View.INVISIBLE
             val authenticationResult = ks.authenticate()
-            if(authenticationResult){
-                Toast.makeText(this.applicationContext, "Accepted", Toast.LENGTH_SHORT).show()
+            if(authenticationResult == 1){
+               // Toast.makeText(this.applicationContext, "Accepted", Toast.LENGTH_SHORT).show()
+                goodResultTextView.visibility = View.VISIBLE
+                Handler(Looper.getMainLooper()).postDelayed({
+                    goodResultTextView.visibility = View.INVISIBLE
+                },5000)
+            }else if(authenticationResult == -1){
+                badPINResultTextView.visibility = View.VISIBLE
+                Handler(Looper.getMainLooper()).postDelayed({
+                    badPINResultTextView.visibility = View.INVISIBLE
+                },5000)
+                //Toast.makeText(this.applicationContext, "Rejected", Toast.LENGTH_SHORT).show()
             }else{
-                Toast.makeText(this.applicationContext, "Rejected", Toast.LENGTH_SHORT).show()
+                badRhythmResultTextView.visibility = View.VISIBLE
+                Handler(Looper.getMainLooper()).postDelayed({
+                    badRhythmResultTextView.visibility = View.INVISIBLE
+                },5000)
             }
-            Log.v("AUTHENTICATIONPROVIDER",authenticationResult.toString())
+            Log.v("AUTHENTICATIONPROVIDER", authenticationResult.toString())
         }
     }
 
